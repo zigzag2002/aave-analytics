6b55f52221f8913867735ab5ed4cf064faaae6b9
zigzag2002 <lukexie2002@gmail.com>
2022-07-08 18:38:21 -0700

    initial commit

---
+++ getTokens.js
@@ -0,0 +1,138 @@
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+
+var admin = require("firebase-admin");
+var serviceAccount = require("./admin.json");
+
+const Web3 = require('web3');
+const abi = require('./abi.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://aave-analytics-default-rtdb.firebaseio.com/"
+});
+
+var db = admin.database();
+
+// initialize eth node
+let add = 'wss://4b5a3125fc584c25b7099611205eeeda.eth.ws.rivet.cloud/'
+var web3 = new Web3(new Web3.providers.WebsocketProvider(add,
+    {
+        clientConfig: {
+            maxReceivedFrameSize: 1000000000,
+            maxReceivedMessageSize: 1000000000,
+        }
+    }));
+
+
+
+async function getPoolData(address) {
+    var tokenRef = db.ref(`aave/pools/${address}/tokens`);
+    const poolContract = new web3.eth.Contract(abi.poolAbi, address);
+    const list = await poolContract.methods.getReservesList().call();
+    for (let address of list) {
+        var assetContract;
+        var assetSymbol;
+        var assetName;
+        if (address === '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2') {
+            assetContract = new web3.eth.Contract(abi.makerToken, address);
+            assetSymbol = web3.utils.hexToAscii(await assetContract.methods.symbol().call());
+            assetName = web3.utils.hexToAscii(await assetContract.methods.name().call());
+        }
+
+        else {
+            assetContract = new web3.eth.Contract(abi.erc20, address);
+            assetSymbol = await assetContract.methods.symbol().call();
+            assetName = await assetContract.methods.name().call();
+        }
+
+        const assetConfiguration = await poolContract.methods.getConfiguration(address).call();
+        const num = BigInt(assetConfiguration[0])
+        const binary = num.toString(2)
+        const strlen = binary.length
+        const loanToValue = parseInt(binary.substring(strlen - 16, strlen), 2)/100
+        const liquidationThreshold = parseInt(binary.substring(strlen - 32, strlen - 16), 2)/100
+        const liquidationBonus = parseInt(binary.substring(strlen - 48, strlen - 32), 2)/100
+        const decimals = parseInt(binary.substring(strlen - 56, strlen - 48), 2)
+        const reserveIsActive = (parseInt(binary[strlen-57]) === 1)
+        const reserveIsFrozen = (parseInt(binary[strlen-58]) === 1)
+        const borrowingIsEnabled = (parseInt(binary[strlen-59]) === 1)
+        const stableRateBorrowingEnabled = (parseInt(binary[strlen-60]) === 1)
+        var reserved = parseInt(binary.substring(strlen-64, strlen-60), 2)
+        var reserveFactor = parseInt(binary.substring(0, strlen - 64), 2)/100
+
+        if (isNaN(reserved)) {reserved = 0}
+        if (isNaN(reserveFactor)) {reserveFactor = 0}
+
+        const assetData = await poolContract.methods.getReserveData(address).call();
+        const aTokenContract = new web3.eth.Contract(abi.erc20, assetData.aTokenAddress);
+        const stableTokenContract = new web3.eth.Contract(abi.erc20, assetData.stableDebtTokenAddress);
+        const variableTokenContract = new web3.eth.Contract(abi.erc20, assetData.variableDebtTokenAddress);
+
+        const aTokenName = await aTokenContract.methods.name().call();
+        const aTokenSupply = await aTokenContract.methods.totalSupply().call();
+        //const aTokenDecimals = await aTokenContract.methods.decimals().call();
+        const aTokenSymbol = await aTokenContract.methods.symbol().call();
+
+        const stableDebt = await stableTokenContract.methods.totalSupply().call();
+        //const stableTokenDecimals = await stableTokenContract.methods.decimals().call();
+        const stableTokenName = await stableTokenContract.methods.name().call()
+        const stableTokenSymbol = await stableTokenContract.methods.symbol().call();
+
+        const variableDebt = await variableTokenContract.methods.totalSupply().call();
+        //const variableDecimals = await variableTokenContract.methods.decimals().call();
+        const variableTokenName = await stableTokenContract.methods.name().call()
+        const variableTokenSymbol = await variableTokenContract.methods.symbol().call();
+
+        var stableDebtAdjusted = stableDebt / (10 ** decimals)
+        var variableDebtAdjusted = variableDebt / (10 ** decimals)
+        var aTokenSupplyAdjusted = aTokenSupply / (10 ** decimals)
+        await tokenRef.update({
+            [assetSymbol.replace(/[^A-Z0-9]+/ig, "")]: {
+                symbol: assetSymbol,
+                name: assetName,
+                supply: aTokenSupplyAdjusted,
+                variableDebt: variableDebtAdjusted,
+                stableDebt: stableDebtAdjusted,
+                totalDebt: stableDebtAdjusted + variableDebtAdjusted,
+                aTokenAddress: assetData.aTokenAddress,
+                aTokenName: aTokenName,
+                aTokenSymbol: aTokenSymbol,
+                stableDebtTokenAddress: assetData.stableDebtTokenAddress,
+                stableTokenName: stableTokenName,
+                stableTokenSymbol: stableTokenSymbol,
+                variableDebtTokenAddress: assetData.variableDebtTokenAddress,
+                variableTokenName: variableTokenName,
+                variableTokenSymbol: variableTokenSymbol,
+                loanToValue: loanToValue,
+                liquidationThreshold: liquidationThreshold,
+                liquidationBonus:liquidationBonus,
+                decimals: decimals,
+                reserveIsActive: reserveIsActive,
+                reserveIsFrozen: reserveIsFrozen,
+                borrowingIsEnabled: borrowingIsEnabled,
+                stableRateBorrowingEnabled: stableRateBorrowingEnabled,
+                reserved: reserved,
+                reserveFactor: reserveFactor,
+                time: (new Date()).toUTCString()
+            }
+        })
+
+    }
+}
+
+
+
+exports.helloPubSub = (event, context) => {
+    return new Promise(function (resolve, reject) {
+        getPoolData('0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9').then(() => {
+            console.log("done")
+            resolve();
+        })
+    })
+
+}; 
---
+++ getVariableTokens.js
@@ -0,0 +1,187 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./admin.json");
+const Web3 = require('web3');
+const abi = require('./abi.json')
+const { promisify } = require('util');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://aave-analytics-default-rtdb.firebaseio.com/"
+});
+const db = admin.database();
+let add = 'wss://4b5a3125fc584c25b7099611205eeeda.eth.ws.rivet.cloud/'
+const web3 = new Web3(new Web3.providers.WebsocketProvider(add,
+    {
+        clientConfig: {
+            maxReceivedFrameSize: 1000000000,
+            maxReceivedMessageSize: 1000000000,
+        }
+    }));
+
+async function getBorrowers(address, startBlock, count, snapshot) {
+    const poolContract = new web3.eth.Contract(abi.poolAbi, '0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9');
+    const currentBlock = await web3.eth.getBlockNumber();
+    const contract = new web3.eth.Contract(abi.erc20, address)
+    const symbol = (await contract.methods.symbol().call()).replace(/[^A-Z0-9]+/gi, '');
+    const dec = await contract.methods.decimals().call()
+    const addressesSearched = []
+    const addressRef = db.ref(`aave/pools/0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9/addresses`);
+    const symbolRef = db.ref(`aave/pools/0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9/tokenHolders/${symbol}`);
+    const tokenRef = db.ref(`aave/pools/0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9/tokenHolders/${symbol}/addresses`);
+    var runningJobs = 0;
+    const getPastEvents = promisify(contract.getPastEvents.bind(contract));
+
+    async function updateAccount(address) {
+        const accountData = await poolContract.methods.getUserAccountData(address).call()
+        await addressRef.child(address).update({
+            totalCollateralETH: parseInt(accountData.totalCollateralETH) / (10 ** 18),
+            totalDebtETH: parseInt(accountData.totalDebtETH) / (10 ** 18),
+            availableBorrowsETH: parseInt(accountData.availableBorrowsETH) / (10 ** 18),
+            currentLiquidationThaccountDatahold: parseInt(accountData.currentLiquidationThreshold) / 100,
+            ltv: parseInt(accountData.ltv) / 100,
+            healthFactor: parseInt(accountData.healthFactor)
+        })
+    }
+
+    async function readEventsRange(start, end) {
+        runningJobs += 1
+        console.log("Event " + start + " to " + end + " for " + symbol)
+        try {
+            const events = await getPastEvents('Transfer', { fromBlock: start, toBlock: end });
+            if (events.length > 0) {
+                await Promise.all(
+                    events
+                        .filter(
+                            (e) =>
+                                (e.returnValues.from !== '0x0000000000000000000000000000000000000000' &&
+                                    !addressesSearched.includes(e.returnValues.from)) || (e.returnValues.to !== '0x0000000000000000000000000000000000000000' &&
+                                        !addressesSearched.includes(e.returnValues.to))
+                        )
+                        .map(async (e) => {
+                            if (e.returnValues.from !== '0x0000000000000000000000000000000000000000' &&
+                                !addressesSearched.includes(e.returnValues.from)) {
+                                addressesSearched.push(e.returnValues.from);
+                                const balance = await contract.methods.balanceOf(e.returnValues.from).call();
+                                const balanceAdjusted = parseInt(balance) / 10 ** dec;
+                                const exists = (!!(snapshot && snapshot.tokenHolders && snapshot.tokenHolders[symbol]
+                                    && snapshot.tokenHolders[symbol].addresses && snapshot.tokenHolders[symbol].addresses[e.returnValues.from] !== null
+                                    && snapshot.tokenHolders[symbol].addresses[e.returnValues.from] !== undefined))
+                                if (parseInt(balance) !== 0) {
+                                    if (!exists) { count++ }
+                                    if (!exists || (!snapshot.tokenHolders[symbol].addresses[e.returnValues.from] === balanceAdjusted)) {
+                                        await tokenRef.update({
+                                            [e.returnValues.from]: balanceAdjusted,
+                                        });
+                                        await addressRef.child(e.returnValues.from + "/tokens").update({
+                                            [symbol]: balanceAdjusted,
+                                        });
+                                        await updateAccount(e.returnValues.from)
+                                    }
+                                } else {
+                                    if (exists) {
+                                        await tokenRef.child(`${e.returnValues.from}`).remove();
+                                        await addressRef.child(`${e.returnValues.from}/tokens/${symbol}`).remove();
+                                        await updateAccount(e.returnValues.from)
+                                        console.log("Removed " + e.returnValues.from + " " + symbol)
+                                        count--;
+                                    }
+                                }
+                            }
+                            if (e.returnValues.to !== '0x0000000000000000000000000000000000000000' &&
+                                !addressesSearched.includes(e.returnValues.to)) {
+                                addressesSearched.push(e.returnValues.to);
+                                const balance = await contract.methods.balanceOf(e.returnValues.to).call();
+                                const balanceAdjusted = parseInt(balance) / 10 ** dec;
+                                const exists = (!!(snapshot && snapshot.tokenHolders && snapshot.tokenHolders[symbol]
+                                    && snapshot.tokenHolders[symbol].addresses && snapshot.tokenHolders[symbol].addresses[e.returnValues.to] !== null
+                                    && snapshot.tokenHolders[symbol].addresses[e.returnValues.to] !== undefined))
+                                if (parseInt(balance) !== 0) {
+                                    if (!exists) { count++ }
+                                    if (!exists || (!snapshot.tokenHolders[symbol].addresses[e.returnValues.from] === balanceAdjusted)) {
+                                        await tokenRef.update({
+                                            [e.returnValues.to]: balanceAdjusted,
+                                        });
+                                        await addressRef.child(e.returnValues.to + "/tokens").update({
+                                            [symbol]: balanceAdjusted,
+                                        });
+                                        await updateAccount(e.returnValues.to)
+                                    }
+                                } else {
+                                    if (exists) {
+                                        await tokenRef.child(`${e.returnValues.to}`).remove();
+                                        await addressRef.child(`${e.returnValues.to}/tokens/${symbol}`).remove();
+                                        await updateAccount(e.returnValues.to)
+                                        console.log("Removed " + e.returnValues.to + " " + symbol)
+                                        count--;
+                                    }
+                                }
+                            }
+                        }),
+                );
+                runningJobs -= 1;
+                if (runningJobs === 0) {
+                    console.log("DONE " + symbol)
+                    await symbolRef.update({
+                        time: (new Date()).toUTCString(),
+                        lastBlock: currentBlock,
+                        count: count
+                    });
+                    console.log("Finished " + symbol)
+                }
+            } else {
+                runningJobs -= 1;
+                if (runningJobs === 0) {
+                    await symbolRef.update({
+                        time: (new Date()).toUTCString(),
+                        lastBlock: currentBlock,
+                        count: count
+                    });
+                    console.log("Finished " + symbol)
+                }
+            }
+        } catch (errors) {
+            console.log(errors.message)
+            if (errors.message.includes('10,000 results')) {
+                const middle = Math.round((start + end) / 2);
+                runningJobs -= 1;
+                return Promise.all([
+                    readEventsRange(start, middle),
+                    readEventsRange(middle + 1, end),
+                ]);
+            }
+            else {
+                await symbolRef.update({
+                    time: (new Date()).toUTCString(),
+                    count: count
+                });
+                console.log("Error - updating count")
+                return;
+            }
+        }
+    }
+    return readEventsRange(startBlock, currentBlock)
+}
+
+exports.helloPubSub = (event, context) => {
+    const ref = db.ref("aave/pools/0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9")
+    return new Promise(function (resolve, reject) {
+        ref.once('value', async (snapshot) => {
+            const promises = []
+            for (let obj of Object.values(snapshot.val().tokens)) {
+                const tokenExists = snapshot.val() && snapshot.val().tokenHolders && snapshot.val().tokenHolders[obj.variableTokenSymbol]
+                const start = tokenExists && snapshot.val().tokenHolders[obj.variableTokenSymbol].lastBlock
+                    ? snapshot.val().tokenHolders[obj.variableTokenSymbol].lastBlock : 0
+                const count = tokenExists && snapshot.val().tokenHolders[obj.variableTokenSymbol].count
+                    ? snapshot.val().tokenHolders[obj.variableTokenSymbol].count : 0
+                promises.push(getBorrowers(obj.variableDebtTokenAddress, start, count, snapshot.val()))
+            }
+            Promise.all(promises).then(() => {
+                console.log("DONE")
+                resolve();
+            })
+        }, (errorObject) => {
+            console.log('The read failed: ' + errorObject.name);
+        });
+    })
+
+};
